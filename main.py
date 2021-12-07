651c464b684bcd7e4cda0ed7c9c88e9c4fc71477
Luiz Valverde <luizcastroval321@gmail.com>
2021-12-07 15:22:45 -0300

    feat:schedule tweet

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+#function that sends the tweet with image
+def sendGarf():
+    api.update_status_with_media("Saturday Baby", "saturdayGarf.png")
+
+#schedule the tweets
+import schedule
+import time
+
+schedule.every().saturday.at("10:00").do(sendGarf)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
